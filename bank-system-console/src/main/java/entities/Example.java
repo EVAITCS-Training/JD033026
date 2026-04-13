d35f6ee2de7a97016e9589b4f264bbb01182bba1
Matthew Jeshua Martin <matthew-martin@programmer.net>
2026-04-13 14:01:24 -0400

    Composite and Design

---
+++ bank-system-console/src/main/java/entities/Example.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class Example {
+    {
+        System.out.println("Instance 1"); // Third Instance
+    }
+
+    {
+        System.out.println("Instance 2");
+    }
+
+    static {
+        System.out.println("Static 1"); // static first
+    }
+
+    public Example() {
+        System.out.println("Constructor 1"); // Constructor Second
+    }
+}
